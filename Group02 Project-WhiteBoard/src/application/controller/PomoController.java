f4560d586ec990393a9030c00eda9787b7928ff1
izzymo34 <81452878+izzymo34@users.noreply.github.com>
2022-04-21 20:59:59 -0500

    Update PomoController.java

--- Group02 Project-WhiteBoard/src/application/controller/PomoController.java
+++ Group02 Project-WhiteBoard/src/application/controller/PomoController.java
@@ -1,3 +1,10 @@
+/**
+ * PomoController is a Java class containing a startTimer method, pauseTimer method, and a goHome method.
+ * 
+ * @author Isabella Montemayor (ynu611)
+ * UTSA CS 3443 - Semester Project
+ * Spring 2022
+ */
 package application.controller;
 
 import java.io.File;
@@ -18,11 +25,11 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class PomoController {
-	@FXML
-	private Button startBtn;
+    @FXML
+    private Button startBtn;
 	
-	@FXML
-	private Button pauseBtn;
+    @FXML
+    private Button pauseBtn;
 	
     private Timer timer = new Timer();
     
@@ -51,7 +58,7 @@ public class PomoController {
     private int minutes;
     private int studySessionNum =1;
     int motivationCount = 0;
-	
+	//When startBtn button is clicked it will start the time and based on the study session count and isBreak boolean
 	@FXML
 	public void startTimer(ActionEvent event) {
         timer = new Timer();
@@ -60,9 +67,10 @@ public class PomoController {
             public void run() {
                 Platform.runLater(() -> {
                     counter--;
-
+		    //Calculations for the default time
                     seconds = counter % 60;
                     minutes = counter / 60;
+		    //If-statement block that will set the timerLbl text with the specified seconds and minutes and formatted accordingly
                     if (seconds < 10 && minutes < 10) {
                         timerLbl.setText("0" + minutes + ":0" + seconds);
                     } else if (minutes < 10) {
@@ -70,7 +78,7 @@ public class PomoController {
                     } else {
                         timerLbl.setText(minutes + ":" + seconds);
                     }
-
+		    //If-statement block that calculates and updates the counter depending on which study session or break the user is on
                     if (counter == 0 && studySessionNum == 4 && !isBreak) { 
                         isBreak = true;
                         counter = 15 * 60; 
@@ -83,10 +91,11 @@ public class PomoController {
                         studySessionNum++;
                         isBreak = false;
                     }
-
                     
                     studySessionLbl.setText("Study Session #" + studySessionNum);
+		    //If-statement to check if the user is on a break or study session to display that the next task is to study else
                     if (isBreak) {
+			//Switch statement to set the whatsNextLbl with a motivational message based on the motivationCount
                     	switch(motivationCount) {
                     	case 1:
                     		whatsNextLbl.setText("Next: Study for 25 minutes - You got this!");
@@ -101,15 +110,14 @@ public class PomoController {
                     		whatsNextLbl.setText("Next: Study for 25 minutes - Stay focused!");
                     		break;
        
-                    	}
-                    	
-                
+                    	}    
                     } else if (studySessionNum == 4) {
                         whatsNextLbl.setText("Next: Long break for 15 minutes!");
                     } else {
                         whatsNextLbl.setText("Next: Short break 5 minutes!");
                     }
                     motivationCount++;
+	            //Resets the motivationCount back to zero if four study session have gone by
                     if(motivationCount == 4) 
                     	motivationCount = 0;
                 });
@@ -117,13 +125,14 @@ public class PomoController {
         }, 0, 1000);
 	}
 	
+	//When the pauseBtn is clicked the timer will pause
 	@FXML
 	public void pauseTimer(ActionEvent event) {
 		timer.cancel();
 	}
 	
+	//When homebtn Button is clicked goHome redirects the user back to the home screen
 	public void goHome(ActionEvent event) throws IOException {
-		//TODO
     	URL url = new File("src/application/view/Home.fxml").toURI().toURL();
     	aPane = FXMLLoader.load(url);
     	Scene scene = new Scene(aPane);
